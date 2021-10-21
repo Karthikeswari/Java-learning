775a08bbb6c86ce1809d1c2d972c78ce70520aad
Karthikeswari Padmanaban <81372980+Karthikeswari@users.noreply.github.com>
2021-10-21 16:56:56 +0530

    Update connectpostgres.java

--- connectpostgres.java
+++ connectpostgres.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 public class connectpostgres {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         try {
     		Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydatabase","postgres","K@rthika27");
     		String sql = "insert into contacts (fname,lname,email)" + " values('Dhan','padmanaban','dhan@hmail.com')";
@@ -26,3 +25,13 @@ public class connectpostgres {
 	
 
 }
+/* A Connection is the session between java application and database. The Connection interface is a factory of Statement, 
+PreparedStatement, and DatabaseMetaData i.e. object of Connection can be used to get the object of Statement and DatabaseMetaData. 
+The Connection interface provide many methods for transaction management like commit(), rollback()
+
+
+Once a connection is obtained we can interact with the database. The JDBC Statement, CallableStatement, and PreparedStatement interfaces define the 
+methods and properties that enable you to send SQL or PL/SQL commands and receive data from your database.
+
+
+A JDBC URL provides a way of identifying a database so that the appropriate driver recognizes it and connects to it*/
